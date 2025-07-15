08ce165e3567e3e8f4fb8dd8f61610cf87699082
Eduard Fonseca <114269444+EdwardFJ@users.noreply.github.com>
2025-07-14 22:40:57 -0600

    changes in "home"

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -40,11 +40,11 @@ export type SiteConfig = {
 
 const siteConfig: SiteConfig = {
     website: 'https://example.com',
-    title: 'Dante',
-    subtitle: 'Minimal Astro.js theme',
+    title: 'Edward Fonseca',
+    subtitle: 'Data Engineer & Data Analyst',
     description: 'Astro.js and Tailwind CSS theme for blog and portfolio by justgoodui.com',
     image: {
-        src: '/dante-preview.jpg',
+        src: '',
         alt: 'Dante - Astro.js and Tailwind CSS theme'
     },
     headerNavLinks: [
@@ -98,8 +98,8 @@ const siteConfig: SiteConfig = {
         }
     ],
     hero: {
-        title: 'Hi There & Welcome to My Corner of the Web!',
-        text: "I'm **Ethan Donovan**, a web developer at Amazing Studio, dedicated to the realms of collaboration and artificial intelligence. My approach involves embracing intuition, conducting just enough research, and leveraging aesthetics as a catalyst for exceptional products. I have a profound appreciation for top-notch software, visual design, and the principles of product-led growth. Feel free to explore some of my coding endeavors on <a href='https://github.com/JustGoodUI/dante-astro-theme'>GitHub</a> or follow me on <a href='https://twitter.com/justgoodui'>Twitter/X</a>.",
+        title: 'This is my web portfolio, a space to explore my projects and skills.!',
+        text: " Hi, my name is Edward Fonseca. I'm passionate about data and work as a Data Engineer & Data Analyst.With a background in managing business operations, I developed strong organizational and problem-solving skills. Over the last year, I have focused on designing scalable data pipelines, cleaning and modeling data for market research, and building interactive dashboards for real-time KPI monitoring. My goal is to turn data into valuable insights that drive better decisions and business growth. I am always eager to learn new technologies and deliver quality results in every project.",
         image: {
             src: '/hero.jpeg',
             alt: 'A person sitting at a desk in front of a computer'
